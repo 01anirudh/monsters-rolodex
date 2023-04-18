a27b825de86f7f03ba04abdc65009db0677dab5d
Anirudh <anirudhkandwal123@gmail.com>
2023-04-18 20:43:40 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
 import { Search } from './components/search-box/search-box.component';
 
-class App extends React.Component{
-  constructor (){
+import './App.css';
+
+class App extends Component {
+  constructor() {
     super();
-    this.state={
-      monsters:[],
-      searchfield:''
+
+    this.state = {
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -19,21 +22,23 @@ class App extends React.Component{
   }
 
   onSearchChange = event => {
-    this.setState({searchfield:event.target.value})
-  }
+    this.setState({ searchField: event.target.value });
+  };
 
-  render(){
-    const {monsters,searchfield} = this.state;
-    const filteredMonsters = monsters.filter(monster=>
-      monster.name.toLowerCase().includes(searchfield.toLowerCase()));
-      return(
-        <div className = "App">
-          <h1>Monster Rolodex</h1>
-          <Search onSearchchange={this.onSearchChange}/>
-          <CardList monsters={filteredMonsters}/>
-        </div>
-  );
-}
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <Search onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
